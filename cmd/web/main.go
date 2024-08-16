cf9ff54362b99693d88d36838cc5532b8b7ff694
Johna210 <solomonjohna21@gmail.com>
2024-08-16 10:44:25 +0300

    Added models.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,45 +1,74 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"johna.net/snippetbox/internal/models"
 )
 
 type application struct {
 	errorLog *log.Logger
-	infoLog *log.Logger
+	infoLog  *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	// Define a new command-line flag with the name 'addr', a default value of ":4000"
 	// and some short help text explaining what the flag controls. The value of the
 	// flag will be stpred in the addr variable at runtime.
-	addr := flag.String("addr",":4000","Http network address")
+	addr := flag.String("addr", ":4000", "Http network address")
+
+	// mysql dsn string
+	dsn := flag.String("dsn", "web:admin@/snippetbox?parseTime=true", "MySql data source name")
 
 	// This reads in the command-line flag value and assigns it to the addr variable
 	flag.Parse()
 
 	// A logger for information
-	infoLog := log.New(os.Stdout,"INFO\t",log.Ldate|log.Ltime )
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
 	// A logger for an error
-	errorLog := log.New(os.Stderr,"Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
-		infoLog: infoLog,
+		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: app.routes(),
+		Handler:  app.routes(),
 	}
 
 	// Server
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
