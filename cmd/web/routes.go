d1c569b91bb01e3862161c02ec12c156c07e9855
Johna210 <solomonjohna21@gmail.com>
2024-08-26 23:08:45 +0300

    Added create template and forms

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,27 +3,34 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	// router / servermux
-	mux := http.NewServeMux()
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
 	// Create a new File Serve
 	// Given relative path
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	// Use the mux.Handle Function to register the file server as the handler
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	router.Handler(http.MethodGet, "/static/*filepath",
+		http.StripPrefix("/static", fileServer))
+
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	// Create a middleware chain containing our 'standard' middleware
 	// which will be used for every request our application receives.
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return standard.Then(mux)
+	return standard.Then(router)
 }
